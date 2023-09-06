261355c40babd8cd5ebb01b3698e565ca88e39a8
LEONHARD LEUNG <2230618@slu.edu.ph>
2023-09-06 21:42:46 +0800

    added sendMessage() method

--- src/prelim/misc/ListOverflowException.java
+++ src/prelim/misc/ListOverflowException.java
@@ -8,4 +8,8 @@ package prelim.misc;
 import java.lang.Throwable;
 
 public class ListOverflowException extends Throwable {
-}
+    public void sendMessage() {
+        System.out.println("- Array overflow detected!!!");
+        System.out.println("- Unable to insert element into array");
+    } // end of sendExceptionMessage method
+} // end of ListOverflowException class
