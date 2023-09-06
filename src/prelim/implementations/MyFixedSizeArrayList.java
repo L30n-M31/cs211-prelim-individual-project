b5e4899b1e2d1a2b2df1e9c104268246e3699582
LEONHARD LEUNG <2230618@slu.edu.ph>
2023-09-06 20:58:08 +0800

    added instance variables, constructor, and createObject() method

--- src/prelim/implementations/MyFixedSizeArrayList.java
+++ src/prelim/implementations/MyFixedSizeArrayList.java
@@ -1,4 +1,31 @@
+/**
+ * @author Leonhard Leung
+ * Date: 09/06/2023
+ */
+
 package prelim.implementations;
 
+import prelim.misc.ListOverflowException;
+
 public class MyFixedSizeArrayList {
-}
+    private String product;
+    private String modelNumber;
+    private String color;
+    private String weight;
+    private MyFixedSizeArrayList[] array = new MyFixedSizeArrayList[5];
+    private MyFixedSizeArrayList element;
+    private int index = 0;
+
+    public MyFixedSizeArrayList(String p, String m, String c, String w) {
+        product = p;
+        modelNumber = m;
+        color = c;
+        weight = w;
+    } // end of constructor
+
+    public void createObject(String p, String m, String c, String w) throws ListOverflowException {
+        element = new MyFixedSizeArrayList(p, m, c, w);
+        array[index] = element;
+        index++;
+    } // end of createObject method
+} // end of MyFixedSizeArrayList class
