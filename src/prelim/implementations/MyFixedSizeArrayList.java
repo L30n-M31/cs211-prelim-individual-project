9b3068b61a800c3676902e71ded0e3d930a00b3a
LEONHARD LEUNG <2230618@slu.edu.ph>
2023-09-06 21:21:43 +0800

    implemented the interface and added the appropriate methods

--- src/prelim/implementations/MyFixedSizeArrayList.java
+++ src/prelim/implementations/MyFixedSizeArrayList.java
@@ -6,14 +6,16 @@
 package prelim.implementations;
 
 import prelim.misc.ListOverflowException;
+import prelim.misc.MyList;
 
-public class MyFixedSizeArrayList {
+import java.util.NoSuchElementException;
+
+public class MyFixedSizeArrayList implements MyList<MyFixedSizeArrayList> {
     private String product;
     private String modelNumber;
     private String color;
     private String weight;
     private MyFixedSizeArrayList[] array = new MyFixedSizeArrayList[5];
-    private MyFixedSizeArrayList element;
     private int index = 0;
 
     public MyFixedSizeArrayList(String p, String m, String c, String w) {
@@ -22,4 +24,44 @@ public class MyFixedSizeArrayList {
         color = c;
         weight = w;
     } // end of constructor
+
+    @Override
+    public int getSize() {
+        return array.length;
+    } // end of getSize method
+
+    @Override
+    public void insert(MyFixedSizeArrayList data) throws ListOverflowException {
+        array[index] = data;
+        index++;
+    } // end of insert method
+
+    @Override
+    public MyFixedSizeArrayList getElement(MyFixedSizeArrayList data) throws NoSuchElementException {
+        for (MyFixedSizeArrayList element : array) {
+            if (element.equals(data))
+                return element;
+        }
+        return null;
+    } // end of getElement method
+
+    @Override
+    public boolean delete(MyFixedSizeArrayList data) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(data)) {
+                array[i] = null;
+                return true;
+            }
+        }
+        return false;
+    } // end of delete method
+
+    @Override
+    public int search(MyFixedSizeArrayList data) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(data))
+                return i;
+        }
+        return -1;
+    } // end of search method
 } // end of MyFixedSizeArrayList class
