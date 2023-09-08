6d42cdb36633ce1b6a8fead0a74d031a3dbb849e
Leonhard Leung <leonhardleung@gmail.com>
2023-09-08 10:15:01 +0800

    added instance variables and default constructor

--- src/prelim/implementations/MyGrowingArrayList.java
+++ src/prelim/implementations/MyGrowingArrayList.java
@@ -1,4 +1,13 @@
 package prelim.implementations;
 
 public class MyGrowingArrayList {
+    private String projectName;
+    private String dateAssigned;
+    private String dateSubmitted;
+
+    public MyGrowingArrayList() {
+        projectName = null;
+        dateAssigned = null;
+        dateSubmitted = null;
+    } // end of default constructor
 }
