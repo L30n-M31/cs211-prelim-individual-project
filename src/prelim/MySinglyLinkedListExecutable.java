521de7ee69aecdebb7adc26e7dcf2ada66cf5cb4
LEONHARD LEUNG <2230618@slu.edu.ph>
2023-09-11 08:17:48 +0800

    added temporary statements

---
+++ src/prelim/MySinglyLinkedListExecutable.java
@@ -0,0 +1,28 @@
+package prelim;
+
+import prelim.implementations.MySinglyLinkedList;
+import prelim.misc.ListOverflowException;
+
+public class MySinglyLinkedListExecutable {
+    public static void main(String[] args) {
+        MySinglyLinkedList<String> singlyList = new MySinglyLinkedList<>();
+
+        String one = "one";
+        String two = "two";
+        String three = "three";
+        String four = "four";
+        String five = "five";
+
+        try {
+            singlyList.insert(one);
+            singlyList.insert(two);
+            singlyList.insert(three);
+            singlyList.insert(four);
+            singlyList.insert(five);
+        } catch (ListOverflowException e) {
+            e.sendMessage();
+        }
+
+        singlyList.print();
+    }
+}
