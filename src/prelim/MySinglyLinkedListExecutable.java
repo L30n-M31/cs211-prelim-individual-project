d33c7b16b07973427bcf40b5f3d20ca1a086744b
LEONHARD LEUNG <2230618@slu.edu.ph>
2023-09-12 22:21:13 +0800

    fixed the insert method

--- src/prelim/MySinglyLinkedListExecutable.java
+++ src/prelim/MySinglyLinkedListExecutable.java
@@ -4,7 +4,7 @@ import prelim.implementations.MySinglyLinkedList;
 import prelim.misc.ListOverflowException;
 
 public class MySinglyLinkedListExecutable {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ListOverflowException{
         MySinglyLinkedList<String> singlyList = new MySinglyLinkedList<>();
 
         String one = "one";
@@ -13,16 +13,11 @@ public class MySinglyLinkedListExecutable {
         String four = "four";
         String five = "five";
 
-        try {
-            singlyList.insert(one);
-            singlyList.insert(two);
-            singlyList.insert(three);
-            singlyList.insert(four);
-            singlyList.insert(five);
-        } catch (ListOverflowException e) {
-            e = new ListOverflowException();
-        }
-
+        singlyList.insert(one);
+        singlyList.insert(two);
+        singlyList.insert(three);
+        singlyList.insert(four);
+        singlyList.insert(five);
         singlyList.print();
     }
 }
