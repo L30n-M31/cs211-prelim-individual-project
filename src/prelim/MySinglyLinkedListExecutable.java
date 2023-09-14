e106e639cc82d83c1798d859c645af905fbdc1f9
Leonhard Leung <leonhardleung@gmail.com>
2023-09-14 22:07:09 +0800

    added a new test statement for the search method

--- src/prelim/MySinglyLinkedListExecutable.java
+++ src/prelim/MySinglyLinkedListExecutable.java
@@ -13,6 +13,7 @@ public class MySinglyLinkedListExecutable {
         String four = "four";
         String five = "five";
 
+        // insert method test
         singlyList.insert(one);
         singlyList.insert(two);
         singlyList.insert(three);
@@ -20,11 +21,16 @@ public class MySinglyLinkedListExecutable {
         singlyList.insert(five);
         singlyList.print();
 
+        // delete method test
         System.out.println();
         singlyList.delete(three);
         singlyList.print();
 
+        // getElement method test
         String element = singlyList.getElement(two);
         System.out.println("\n" + element);
+
+        // search method test
+        System.out.println("\nElement found at index: " + singlyList.search(four) + "\t" + "position: " + (singlyList.search(four) + 1));
     }
 }
