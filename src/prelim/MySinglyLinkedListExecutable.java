3e352d83711353c181ee5ccff39c857020fd5a80
Leonhard Leung <leonhardleung@gmail.com>
2023-09-15 10:23:21 +0800

    finished the program (to be debugged later):

--- src/prelim/MySinglyLinkedListExecutable.java
+++ src/prelim/MySinglyLinkedListExecutable.java
@@ -7,34 +7,91 @@ package prelim;
 
 import prelim.implementations.MySinglyLinkedList;
 
+import java.util.Scanner;
+
 public class MySinglyLinkedListExecutable {
+
+    private final Scanner keyboard = new Scanner(System.in);
+    MySinglyLinkedList<String> singlyList = new MySinglyLinkedList<>();
+
     public static void main(String[] args) {
-        MySinglyLinkedList<String> singlyList = new MySinglyLinkedList<>();
+        MySinglyLinkedListExecutable execute = new MySinglyLinkedListExecutable();
+        execute.run();
+    } // end of main method
 
-        String one = "one";
-        String two = "two";
-        String three = "three";
-        String four = "four";
-        String five = "five";
+    public void run() {
 
-        // insert method test
-        singlyList.insert(one);
-        singlyList.insert(two);
-        singlyList.insert(three);
-        singlyList.insert(four);
-        singlyList.insert(five);
-        singlyList.print();
+        int selection = 0;
+        while (selection != 5) {
+            menu();
+            selection = Integer.parseInt(readString("Enter among the choices above: "));
+            switch (selection) {
+                case 1 -> addStudent();
+                case 2 -> removeStudent();
+                case 3 -> locateStudent();
+                case 4 -> showStudents();
+            }
+        }
+        System.out.println("\nExiting...");
+        System.exit(0);
+    } // end of run method
 
-        // delete method test
-        System.out.println();
-        singlyList.delete(three);
-        singlyList.print();
+    public void addStudent() {
+        System.out.println("\nADD A STUDENT");
+        do {
+            String name = readString(">>> Enter student's name: ");
+            singlyList.insert(name);
 
-        // getElement method test
-        String element = singlyList.getElement(two);
-        System.out.println("\n" + element);
+            System.out.print("\nDo you want to add another student? <y/n>: ");
+        } while (keyboard.nextLine().equalsIgnoreCase("y"));
+    } // end of addStudent method
 
-        // search method test
-        System.out.println("\nElement found at index: " + singlyList.search(four) + "\t" + "position: " + (singlyList.search(four) + 1));
-    }
+    public void removeStudent() {
+        System.out.println("REMOVE A STUDENT");
+        do {
+            showStudents();
+            String name = readString(">>> Name of student: ");
+            System.out.println(singlyList.delete(name) ? ("\n- " + name + " has been deleted") :
+                    ("\n- " + name + " has not been deleted"));
+            System.out.print("\nDo you want to remove another student? <y/n>: ");
+        } while (keyboard.nextLine().equalsIgnoreCase("y"));
+    } // end of removeStudent method
+
+    public void locateStudent() {
+        System.out.println("\nFIND STUDENT");
+        String name = readString(">>> Name of student: ");
+
+        String element = singlyList.getElement(name);
+        int index = singlyList.search(element);
+        if (index != -1)
+            System.out.println("\n- found a match at position " + (index + 1));
+    } // end of locateStudent method
+
+    public void showStudents() {
+        System.out.println("\n--------------------------------");
+        System.out.println("         Student List           ");
+        System.out.println("--------------------------------");
+        for (int index = 0; index < singlyList.getSize(); index++) {
+            String element = singlyList.getElement(index);
+            System.out.println("\t" + (index + 1) + ". " + element);
+        }
+        System.out.println("--------------------------------\n");
+    } // end of showStudents method
+
+    public String readString(String promptMessage) {
+        System.out.print(promptMessage);
+        return keyboard.nextLine();
+    } // end of readString method
+
+    public void menu() {
+        System.out.println("=========================================");
+        System.out.println("|              MAIN MENU                |");
+        System.out.println("|   ---------------------------------   |");
+        System.out.println("|     1. Add a student to list          |");
+        System.out.println("|     2. Remove a student from list     |");
+        System.out.println("|     3. Find a student from list       |");
+        System.out.println("|     4. View list of students          |");
+        System.out.println("|     5. Exit program                   |");
+        System.out.println("=========================================");
+    } // end of menu method
 }
