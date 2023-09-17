48abacf8daac4b857ff4b665292e33e22b4abe01
Leonhard Leung <leonhardleung@gmail.com>
2023-09-17 20:18:45 +0800

    completed the code

--- src/prelim/MyDoublyLinkedListExecutable.java
+++ src/prelim/MyDoublyLinkedListExecutable.java
@@ -1,4 +1,198 @@
 package prelim;
 
+import prelim.implementations.MyDoublyLinkedList;
+import prelim.misc.Node;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class MyDoublyLinkedListExecutable {
-}
+
+    private final Scanner keyboard = new Scanner(System.in);
+    MyDoublyLinkedList<Book> doublyList = new MyDoublyLinkedList<>();
+
+    public static void main(String[] args) {
+        MyDoublyLinkedListExecutable execute = new MyDoublyLinkedListExecutable();
+        try {
+            execute.run();
+        } catch (NoSuchElementException e) {
+            System.out.println("\n- element not found\n");
+            System.out.println("Exiting...\n");
+            e.printStackTrace();
+        }
+    } // end of main method
+
+    public void run() {
+        int selection = 0;
+        while (selection != 5) {
+            menu();
+            selection = Integer.parseInt(readString("Enter among the choices above: "));
+            switch (selection) {
+                case 1 -> addBook();
+                case 2 -> removeBook();
+                case 3 -> findBook();
+                case 4 -> showList(true);
+            }
+        }
+        System.out.println("\nExiting...");
+        System.exit(0);
+    } // end of run method
+
+    public void addBook() {
+        String borrower, dateBorrowed, bookTitle, bookAuthor, ISBN;
+
+        System.out.println("\nADD A BOOK");
+        do {
+            borrower = readString(">>> Enter borrower name: ");
+            dateBorrowed = readString(">>> Enter date borrowed: ");
+            bookTitle = readString(">>> Enter book title: ");
+            bookAuthor = readString(">>> Enter book author: ");
+            ISBN = readString(">>> Enter ISBN: ");
+
+            doublyList.insert(new Book(borrower, dateBorrowed, bookTitle, bookAuthor, ISBN));
+
+            System.out.print("\nDo you want to add another borrower? <y/n>: ");
+        } while (keyboard.nextLine().equalsIgnoreCase("y"));
+    } // end of addBook method
+
+    public void removeBook() {
+        String bookTitle, bookAuthor;
+
+        System.out.println("\nREMOVE A BOOK");
+        do {
+            showList(false);
+            bookTitle = readString(">>> Book title: ");
+            bookAuthor = readString(">>> Book author: ");
+
+            boolean successfulDeletion = doublyList.delete(doublyList.getElement(new Book(bookTitle, bookAuthor)));
+            System.out.println(successfulDeletion ? ("\n- " + bookTitle + " has been removed") :
+                    ("\n- " + bookTitle + " has not been removed"));
+
+            System.out.print("\nDo you want to remove another borrowed book? <y/n>: ");
+        } while (keyboard.nextLine().equalsIgnoreCase("y"));
+    } // end of removeBook method
+
+    public void findBook() {
+        String bookTitle, bookAuthor;
+
+        System.out.println("\nFIND A BOOK");
+        do {
+            bookTitle = readString(">>> Book title: ");
+            bookAuthor = readString(">>> Book author: ");
+
+            Book book = doublyList.getElement(new Book(bookTitle, bookAuthor));
+
+            int index = doublyList.search(book);
+            if (index != -1) {
+                System.out.println("\n- found a match at position " + (index + 1) + " from the list");
+                System.out.println("\nDetails");
+                System.out.println("============================");
+                System.out.println(book.displayDetails());
+            }
+            System.out.print("Do you want to find another book? <y/n>: ");
+        } while (keyboard.nextLine().equalsIgnoreCase("y"));
+    } // end of findBook method
+
+    public String readString(String promptMessage) {
+        System.out.print(promptMessage);
+        return keyboard.nextLine();
+    } // end of readString method
+
+    public void menu() {
+        System.out.println("==========================================");
+        System.out.println("|        Library Borrowing System        |");
+        System.out.println("|   ----------------------------------   |");
+        System.out.println("|     1. Add a book to the list          |");
+        System.out.println("|     2. Remove a book from the list     |");
+        System.out.println("|     3. Find a book from the list       |");
+        System.out.println("|     4. View list of books              |");
+        System.out.println("|     5. Exit program                    |");
+        System.out.println("==========================================");
+    } // end of menu method
+
+    public void showList(boolean trigger) {
+        boolean displayLatestFirst = false;
+        Node<Book> bookList = doublyList.getLastNode(false);
+        if (trigger) {
+            displayLatestFirst = (readString("\n>>> Display from latest borrowed book? <y/n>: ")
+                    .equalsIgnoreCase("y"));
+            bookList = doublyList.getLastNode(displayLatestFirst);
+        }
+
+        System.out.println("\n-------------------------------------------------------------------------------------");
+        System.out.println("                              List of Borrowed Books                                 ");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.printf("%-20s%-18s%-15s%-20s%-15s%n", "Borrower", "Date Borrowed", "Book Title",
+                "Book Author", "ISBN");
+        System.out.printf("%-20s%-18s%-15s%-20s%-15s%n", "=================", "===============", "============",
+                "=================", "============");
+        for (int index = 0; index < doublyList.getSize(); index++) {
+            Book element = bookList.getData();
+            if (displayLatestFirst)
+                bookList = bookList.getPrevious();
+            else
+                bookList = bookList.getNext();
+            System.out.printf("%-20s%-18s%-15s%-20s%-15s%n", element.getBorrower(), element.getDateBorrowed(),
+                    element.getTitle(), element.getAuthor(), element.getISBN());
+        }
+        System.out.println("-------------------------------------------------------------------------------------\n");
+    } // end of showList method
+
+    /**
+     * This class holds the details of a borrowed book such as name of borrower, date borrowed, book title,
+     * book author, and ISBN
+     */
+    private static class Book {
+        private String borrower;
+        private String dateBorrowed;
+        private final String title;
+        private final String author;
+        private String ISBN;
+
+        public Book(String b, String dB, String t, String a, String i) {
+            borrower = b;
+            dateBorrowed = dB;
+            title = t;
+            author = a;
+            ISBN = i;
+        } // end of constructor
+
+        public Book(String t, String a) {
+            title = t;
+            author = a;
+        } // end of constructor
+
+        public String getBorrower() {
+            return borrower;
+        }
+
+        public String getDateBorrowed() {
+            return dateBorrowed;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public String getISBN() {
+            return ISBN;
+        }
+
+        public String displayDetails() {
+            return "Borrower: " + this.getBorrower() + "\n" +
+                    "Date borrowed: " + this.getDateBorrowed() + "\n" +
+                    "Title: " + this.getTitle() + "\n" +
+                    "Author: " + this.getAuthor() + "\n" +
+                    "ISBN: " + this.getISBN() + "\n";
+        } // end of displayDetails method
+
+        @Override
+        public String toString() {
+            return title + "," + author;
+        } // end of toString method
+    } // end of Book class
+} // end of MyDoublyLinkedListExecutable class
--- src/prelim/implementations/MyDoublyLinkedList.java
+++ src/prelim/implementations/MyDoublyLinkedList.java
@@ -2,69 +2,86 @@ package prelim.implementations;
 
 import prelim.misc.ListOverflowException;
 import prelim.misc.MyList;
+import prelim.misc.Node;
 
 import java.util.NoSuchElementException;
 
 public class MyDoublyLinkedList<T> implements MyList<T> {
-    private T data;
-    private MyDoublyLinkedList<T> next;
-    private MyDoublyLinkedList<T> previous;
+    private Node<T> next;
     private int size = 0;
-
-    public MyDoublyLinkedList() {
-        data = null;
-        next = null;
-        previous = null;
-    }
-
-    public MyDoublyLinkedList(T data) {
-        this.data = data;
-        previous = next;
-        next = null;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setNext(MyDoublyLinkedList<T> node) {
-        next = node;
-    }
-
-    public void setPrevious(MyDoublyLinkedList<T> node) {
-        previous = node;
-    }
-
-    public MyDoublyLinkedList<T> getNext() {
-        return next;
-    }
-
-    public MyDoublyLinkedList<T> getPrevious() {
-        return previous;
-    }
 
     @Override
     public int getSize() {
         return size;
-    }
+    } // end of getSize method
 
     @Override
-    public void insert(T data) throws ListOverflowException {
-
-    }
+    public void insert(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (next == null) {
+            next = newNode;
+        }
+        else {
+            Node<T> currentPointer = next;
+            while (currentPointer.getNext() != null) {
+                currentPointer = currentPointer.getNext();
+            }
+            currentPointer.setNext(newNode);
+            newNode.setPrevious(currentPointer);
+        }
+        size++;
+    } // end of insert method
 
     @Override
     public T getElement(T data) throws NoSuchElementException {
-        return null;
-    }
+        Node<T> currentPointer = next;
+        for (int i = 0; i < getSize(); i++) {
+            if (currentPointer.getData().toString().equalsIgnoreCase(data.toString()))
+                return currentPointer.getData();
+            currentPointer = currentPointer.getNext();
+        }
+        throw new NoSuchElementException();
+    } // end of getElement method
 
     @Override
     public boolean delete(T data) {
+        Node<T> currentPointer = next;
+        Node<T> previousPointer = next;
+        for (int i = 0; i < getSize(); i++) {
+            if (i == 0 && currentPointer.getData().equals(data)) {
+                next = currentPointer.getNext();
+                size--;
+                return true;
+            }
+            if (currentPointer.getData().equals(data)) {
+                previousPointer.setNext(currentPointer.getNext());
+                //currentPointer.getNext().setPrevious(previousPointer);
+                size--;
+                return true;
+            }
+            previousPointer = currentPointer;
+            currentPointer = currentPointer.getNext();
+        }
         return false;
-    }
+    } // end of delete method
 
     @Override
     public int search(T data) {
-        return 0;
-    }
-}
+        Node<T> currentPointer = next;
+        for (int i = 0; i < getSize(); i++) {
+            if (currentPointer.getData().equals(data))
+                return i;
+            currentPointer = currentPointer.getNext();
+        }
+        return -1;
+    } // end of search method
+    public Node<T> getLastNode(boolean displayLatestFirst) {
+        Node<T> current = next;
+        if (displayLatestFirst) {
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+        }
+        return current;
+    } // end of display method
+} // end of MyDoublyLinkedList<T> class
