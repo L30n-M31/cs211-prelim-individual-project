7040072aaa6f6090cc47cc91cd14de38205573b4
Leonhard Leung <leonhardleung@gmail.com>
2023-09-17 20:17:59 +0800

    added comments of each method

--- src/prelim/MySinglyLinkedListExecutable.java
+++ src/prelim/MySinglyLinkedListExecutable.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class MySinglyLinkedListExecutable {
 
     private final Scanner keyboard = new Scanner(System.in);
-    MySinglyLinkedList<StudentList> singlyList = new MySinglyLinkedList<>();
+    MySinglyLinkedList<Student> singlyList = new MySinglyLinkedList<>();
 
     public static void main(String[] args) {
         MySinglyLinkedListExecutable execute = new MySinglyLinkedListExecutable();
@@ -26,6 +26,9 @@ public class MySinglyLinkedListExecutable {
         }
     } // end of main method
 
+    /**
+     * Method that directs the program to a specific operation
+     */
     public void run() {
         int selection = 0;
         while (selection != 5) {
@@ -42,6 +45,9 @@ public class MySinglyLinkedListExecutable {
         System.exit(0);
     } // end of run method
 
+    /**
+     * Method that adds a student to a list
+     */
     public void addStudent() {
         String firstName, lastName, IDNumber;
 
@@ -51,12 +57,15 @@ public class MySinglyLinkedListExecutable {
             lastName = readString(">>> Enter last name: ");
             IDNumber = readString(">>> Enter ID number: ");
 
-            singlyList.insert(new StudentList(firstName, lastName, IDNumber));
+            singlyList.insert(new Student(firstName, lastName, IDNumber));
 
             System.out.print("\nDo you want to add another student? <y/n>: ");
         } while (keyboard.nextLine().equalsIgnoreCase("y"));
     } // end of addStudent method
 
+    /**
+     * Method that removes a student in a list
+     */
     public void removeStudent() {
         String firstName, lastName;
 
@@ -66,7 +75,7 @@ public class MySinglyLinkedListExecutable {
             firstName = readString(">>> First name: ");
             lastName = readString(">>> Last name: ");
 
-            boolean successfulDeletion = singlyList.delete(singlyList.getElement(new StudentList(firstName, lastName)));
+            boolean successfulDeletion = singlyList.delete(singlyList.getElement(new Student(firstName, lastName)));
             System.out.println(successfulDeletion ? ("\n- " + firstName + " has been removed") :
                     ("\n- " + firstName + " has not been removed"));
 
@@ -74,6 +83,9 @@ public class MySinglyLinkedListExecutable {
         } while (keyboard.nextLine().equalsIgnoreCase("y"));
     } // end of removeStudent method
 
+    /**
+     * Method that searches and displays the element in a list
+     */
     public void findStudent() {
         String firstName, lastName;
 
@@ -82,7 +94,7 @@ public class MySinglyLinkedListExecutable {
             firstName = readString(">>> First name: ");
             lastName = readString(">>> Last name: ");
 
-            StudentList student = singlyList.getElement(new StudentList(firstName, lastName));
+            Student student = singlyList.getElement(new Student(firstName, lastName));
 
             int index = singlyList.search(student);
             if (index != -1) {
@@ -95,23 +107,34 @@ public class MySinglyLinkedListExecutable {
         } while (keyboard.nextLine().equalsIgnoreCase("y"));
     } // end of findStudent method
 
+    /**
+     * Method that reads the input of the user
+     * @param promptMessage message to be shown to the user
+     * @return input of the user of type String
+     */
     public String readString(String promptMessage) {
         System.out.print(promptMessage);
         return keyboard.nextLine();
     } // end of readString method
 
+    /**
+     * Method that displays the menu
+     */
     public void menu() {
-        System.out.println("=========================================");
-        System.out.println("|              MAIN MENU                |");
-        System.out.println("|   ---------------------------------   |");
-        System.out.println("|     1. Add a student to list          |");
-        System.out.println("|     2. Remove a student from list     |");
-        System.out.println("|     3. Find a student from list       |");
-        System.out.println("|     4. View list of students          |");
-        System.out.println("|     5. Exit program                   |");
-        System.out.println("=========================================");
+        System.out.println("=============================================");
+        System.out.println("|          Student Management List          |");
+        System.out.println("|   -------------------------------------   |");
+        System.out.println("|     1. Add a student to the list          |");
+        System.out.println("|     2. Remove a student from the list     |");
+        System.out.println("|     3. Find a student from list           |");
+        System.out.println("|     4. View list of students              |");
+        System.out.println("|     5. Exit program                       |");
+        System.out.println("=============================================");
     } // end of menu method
 
+    /**
+     * Method that displays the list
+     */
     public void showList() {
         System.out.println("\n------------------------------------------");
         System.out.println("               Student List               ");
@@ -119,24 +142,27 @@ public class MySinglyLinkedListExecutable {
         System.out.printf("%-15s%-16s%-14s%n", "First Name", "Last Name", "ID Number");
         System.out.printf("%-15s%-16s%-14s%n", "============", "=============", "===========");
         for (int index = 0; index < singlyList.getSize(); index++) {
-            StudentList element = singlyList.getElement(index);
+            Student element = singlyList.getElement(index);
             System.out.printf("%-15s%-16s%-14s%n", element.getFirstName(), element.getLastName(), element.getIDNumber());
         }
         System.out.println("------------------------------------------\n");
     } // end of showList method
 
-    private static class StudentList {
+    /**
+     * This class holds the details of a student such as first name, last name, and id number
+     */
+    private static class Student {
         private final String firstName;
         private final String lastName;
         private String IDNumber;
 
-        public StudentList(String fN, String lN, String ID) {
+        public Student(String fN, String lN, String ID) {
             firstName = fN;
             lastName = lN;
             IDNumber = ID;
         } // end of constructor
 
-        public StudentList(String fN, String lN) {
+        public Student(String fN, String lN) {
             firstName = fN;
             lastName = lN;
         } // end of constructor
@@ -153,14 +179,22 @@ public class MySinglyLinkedListExecutable {
             return IDNumber;
         }
 
+        /**
+         * Method that displays the details of an element in a list
+         * @return element details condensed in a String
+         */
         public String displayDetails() {
             return "Name: " + this.getFirstName() + " " + this.getLastName() + "\n" +
                     "ID number: " + this.getIDNumber() + "\n";
-        } // end of displayDetails
+        } // end of displayDetails method
 
+        /**
+         * Override method that returns a string to be used for comparing data
+         * @return csv type format of chosen variables of an element
+         */
         @Override
         public String toString() {
             return firstName + "," + lastName;
         } // end of toString method
-    } // end of StudentList class
+    } // end of Student class
 } // end of MySinglyLinkedListExecutable class
