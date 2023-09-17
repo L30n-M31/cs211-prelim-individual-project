eebf4d2a46951d32b2dc87a75e2a8328f4096ac7
Leonhard Leung <leonhardleung@gmail.com>
2023-09-17 15:09:05 +0800

    added a Node<T> class

---
+++ src/prelim/misc/Node.java
@@ -0,0 +1,44 @@
+/**
+ * @author Leonhard Leung
+ * Date: 09/17/2023
+ */
+
+package prelim.misc;
+
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+    private Node<T> previous;
+
+    public Node() {
+        data = null;
+        next = null;
+        previous = null;
+    }
+
+    public Node(T data) {
+        this.data = data;
+        next = null;
+        previous = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setNext(Node<T> node) {
+        next = node;
+    }
+
+    public void setPrevious(Node<T> node) {
+        previous = node;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public Node<T> getPrevious() {
+        return previous;
+    }
+} // end of Node<T> class
